f7645afee5457f2ae61b6978285c8b22ec49a313
Nqv4 <109492317+NQV4@users.noreply.github.com>
2023-06-18 16:34:54 +0900

    Update content.js

--- content.js
+++ content.js
@@ -1,1 +1,6 @@
-
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('saveButton').addEventListener('click', saveURL);
+    document.getElementById('loadButton').addEventListener('click', loadURLs);
+    loadURLs(); // ページロード時にURLリストを読み込む
+  });
+  
